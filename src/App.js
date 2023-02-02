bc3f01516295f373f97a4e123e03f8052eda89f4
Muhammed saheerkhan <muhammedsaheerkhan@Saheers-Air.Dlink>
2023-02-02 21:58:11 +0530

    Package single page compeleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./components/screens/Home/Spotlight/Home";
 import { Helmet } from "react-helmet";
 import Cart from "./components/screens/Cart/Cart";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PackageSingle from "./components/screens/Package/PackageSingle/PackageSingle";
 
 function App() {
@@ -10,21 +10,21 @@ function App() {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>HimTrek - Your Travelling Partner </title>
+        <title>HimTrek - Your Traveling Partner </title>
         <link
           rel="icon"
           href={require("./components/assets/images/himtrek-logo.svg").default}
         />
       </Helmet>
       <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route  path='/cart/' element={<Cart />} />
-          <Route  path='/package-single/' element={<PackageSingle />} />
-        </Routes>
-      </Router>
-    </div>
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/cart/' element={<Cart />} />
+            <Route path='/package-single/' element={<PackageSingle />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
