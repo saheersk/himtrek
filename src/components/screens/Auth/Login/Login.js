35d19001b0b8838bf74257deeb92614b28741cc3
dilshanmambadan <dilshanmambadan@gmail.com>
2023-02-21 20:06:54 +0530

    pages updated

--- src/components/screens/Auth/Login/Login.js
+++ src/components/screens/Auth/Login/Login.js
@@ -19,7 +19,7 @@ function Login() {
                 <input type="text" />
                 <label htmlFor="">Password</label>
                 <input type="text" />
-                <span>Forgot Your Password</span>
+                <span>Forgot Your Password ?</span>
                 <input type="submit" value="Login" />
               </form>
               <span>Create Account</span>
